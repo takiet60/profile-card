c9dc942b91295bd27fa32a5d64c57a55fc4e26ab
TaAnhKiet <18130119@st.hcmuaf.edu.vn>
2022-10-03 15:07:08 +0700

    Complete code for design

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import image from './constants'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main className="wrapper">
+            <img src={image.patternTop} className="pattern--top" alt="pattern top" />
+            <img src={image.patternBottom} alt="pattern bottom" className="pattern--bottom" />
+            <article className="card">
+                <header className='card__header'>
+                    <img src={image.avatar} className='card__avatar' alt="user avatar" />
+                </header>
+                <div className="card__body">
+                    <div className="card__info d-flex justify-center gap-1">
+
+                        <p className="card__name b-700">
+                            Victor Crest
+                        </p>
+                        <p className="card__age gray">
+                            26
+                        </p>
+                    </div>
+                    <p className="card__location gray">
+                        London
+                    </p>
+                </div>
+                <footer className='card__footer d-flex justify-evenly'>
+                    <span className="card__social text-center">
+                        <p className="card__number b-700">80K
+                        </p>
+                        <p className="card__media gray font-sm letter-spacing-1">Followers
+                        </p>
+                    </span>
+                    <span className="card__social text-center">
+                        <p className="card__number b-700">803K
+                        </p>
+
+                        <p className="card__media gray font-sm letter-spacing-1">Likes
+                        </p>
+                    </span>
+                    <span className="card__social text-center">
+
+                        <p className="card__number b-700">1.4K
+                        </p>
+                        <p className="card__media gray font-sm letter-spacing-1">Photos
+                        </p>
+                    </span>
+                </footer>
+            </article>
+        </main>
+    );
 }
 
 export default App;
---
+++ src/constants/image.js
@@ -0,0 +1,9 @@
+import avatar from '../images/image-victor.jpg'
+import patternTop from '../images/bg-pattern-top.svg'
+import patternBottom from '../images/bg-pattern-bottom.svg'
+
+export default {
+    avatar,
+    patternBottom,
+    patternTop
+}
